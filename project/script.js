5f8f4d5e694b1cd0c033011a6e0b708580555e25
Tim von Asen <timvonasen@users.noreply.github.com>
2022-12-14 13:21:26 +0100

    Todo redu

--- project/script.js
+++ project/script.js
@@ -1,18 +1,7 @@
-const message = "More information can be found by clicking on the letters.";
-alert(message);
-
-const showSlideTree = () => {
-  let actionSlidePaper = document.querySelector(".slidePaper");
-  actionSlidePaper.style.display = "none";
-
-  let actionSlideTree = document.querySelector(".slideTree");
-  actionSlideTree.style.display = "flex";
-};
-
-const showSlidePaper = () => {
-  let actionSlideTree = document.querySelector(".slideTree");
-  actionSlideTree.style.display = "none";
-
-  let actionSlidePaper = document.querySelector(".slidePaper");
-  actionSlidePaper.style.display = "flex";
-};
+//draw lines with margin of 1 px
+var margin = 1;
+var line = new Path.Line({
+  from: [margin, margin],
+  to: [view.size.width - margin, view.size.height - margin],
+  strokeColor: "black",
+});
