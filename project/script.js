72c0afc0a73b8237c60eb8282100365d91d66dbb
Ivy5418 <75975090+Ivy5418@users.noreply.github.com>
2022-12-28 13:58:24 +0100

    update
    feinschliff

--- project/script.js
+++ project/script.js
@@ -1,1 +1,7 @@
-
+//draw lines with margin of 1 px
+var margin = 1;
+var line = new Path.Line({
+  from: [margin, margin],
+  to: [view.size.width - margin, view.size.height - margin],
+  strokeColor: "black",
+});
