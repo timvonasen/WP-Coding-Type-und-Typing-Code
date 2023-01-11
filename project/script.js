8cc299f0d05ce52f853656314a710ce7ce8d4a07
Tim von Asen <timvonasen@users.noreply.github.com>
2023-01-11 10:23:42 +0100

    Final

--- project/script.js
+++
@@ -1,7 +0,0 @@
-//draw lines with margin of 1 px
-var margin = 1;
-var line = new Path.Line({
-  from: [margin, margin],
-  to: [view.size.width - margin, view.size.height - margin],
-  strokeColor: "black",
-});
